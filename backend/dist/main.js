940f504d6e0a0ad8e3c844a8c4c4e73e2a7efbd2
arpangoyal01 <goyal.arpan01@gmail.com>
2026-05-31 12:38:28 +0530

    added helmet

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -1,11 +1,16 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const common_1 = require("@nestjs/common");
+const helmet_1 = __importDefault(require("helmet"));
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.useGlobalPipes(new common_1.ValidationPipe());
+    app.use((0, helmet_1.default)());
     app.enableCors();
     await app.listen(process.env.Port || 3000);
 }
